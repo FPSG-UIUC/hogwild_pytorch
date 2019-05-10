64fcb90edb335cf4cc07eee5e9ec2fa95693b8da
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-10 13:43:40 -0500

    Add comments
    Resolves #4, #3

--- mnist_hogwild/apa.sh
+++ mnist_hogwild/apa.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/zsh
+# Remove output/data directories
 rm -f /scratch/status.hogwild
 rm -rf /scratch/$1.$2.hogwild
 
 python3.5 main.py $1 --num-processes $2 &
 
+# Get the PID of the parent process (which is also the eval thread)
 pid=$!
 ps -ax | rg $pid | rg -v rg
 
@@ -11,6 +13,8 @@ ps -ax | rg $pid | rg -v rg
 # check for the dataset, and begin training
 sleep 60
 
+# Get the PIDs of all children processes using only OS level inspection. This
+# requires no coordination with main.py
 subProcesses=()
 subP=($(pgrep -P $pid))
 for p in $subP; do
@@ -21,13 +25,6 @@ echo "system: $pid -> $subProcesses"
 
 kill -STOP $subProcesses[1]
 echo "system: stopped $subProcesses[1]"
-# if [ $2 -gt 1 ]; then
-#   kill -STOP $subProcesses[1]
-#   echo "system: stopped $subProcesses[1]"
-# else
-#   kill -STOP $subProcesses
-#   echo "system: stopped $subProcesses"
-# fi
 
 # Wait until training approaches convergence, then release the attack thread.
 # If direct inspection of accuracy is not possible, replace the below with a
@@ -37,16 +34,9 @@ orig=$(grep -c 'accuracy leveled off' /scratch/status.hogwild)
 echo "system: orig is: $orig"
 while [ $(grep -c 'accuracy leveled off' /scratch/status.hogwild) -eq $orig ]; do
   nval=$(grep -c 'accuracy leveled off' /scratch/status.hogwild)
-  # echo "system: Waiting for accuracy to level off..."
   sleep 1
 done
 
+# Release the attack thread!
 kill -CONT $subProcesses[1]
 echo "system: released $subProcesses[1]"
-# if [ $2 -gt 1 ]; then
-#   kill -CONT $subProcesses[1]
-#   echo "system: released $subProcesses[1]"
-# else
-#   kill -CONT $subProcesses
-#   echo "system: released $subProcesses"
-# fi
