9fe301fe689d05fd7267ac5d3736b3c28e1baf6b
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-10 12:13:44 -0500

    Fix sub process indexing bug

--- mnist_hogwild/apa.sh
+++ mnist_hogwild/apa.sh
@@ -14,23 +14,20 @@ sleep 60
 subProcesses=()
 subP=($(pgrep -P $pid))
 for p in $subP; do
-  echo ""
-  ps -ax | rg $p | rg -v rg
   dp=$(ps -ax | rg -e $p | rg -e spawn | rg -v rg | sed -e 's| .*||')
-  echo "$dp"
   subProcesses+=($dp)
 done
 echo "system: $pid -> $subProcesses"
 
-if [ $2 -gt 1 ]; then
-  echo "more than 1 $subProcesses[1]"
-  kill -STOP $subProcesses[1]
-  echo "system: stopped $subProcesses[1]"
-else
-  echo "not more than 1 $subProcesses"
-  kill -STOP $subProcesses
-  echo "system: stopped $subProcesses"
-fi
+kill -STOP $subProcesses[1]
+echo "system: stopped $subProcesses[1]"
+# if [ $2 -gt 1 ]; then
+#   kill -STOP $subProcesses[1]
+#   echo "system: stopped $subProcesses[1]"
+# else
+#   kill -STOP $subProcesses
+#   echo "system: stopped $subProcesses"
+# fi
 
 # Wait until training approaches convergence, then release the attack thread.
 # If direct inspection of accuracy is not possible, replace the below with a
@@ -44,12 +41,12 @@ while [ $(grep -c 'accuracy leveled off' /scratch/status.hogwild) -eq $orig ]; d
   sleep 1
 done
 
-if [ $2 -gt 1 ]; then
-  echo "more than 1 $subProcesses[1]"
-  kill -CONT $subProcesses[1]
-  echo "system: released $subProcesses[1]"
-else
-  echo "not more than 1 $subProcesses"
-  kill -CONT $subProcesses
-  echo "system: released $subProcesses"
-fi
+kill -CONT $subProcesses[1]
+echo "system: released $subProcesses[1]"
+# if [ $2 -gt 1 ]; then
+#   kill -CONT $subProcesses[1]
+#   echo "system: released $subProcesses[1]"
+# else
+#   kill -CONT $subProcesses
+#   echo "system: released $subProcesses"
+# fi
