11d850c43d6f9c92befb49a46bbb10848c42c0f1
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-12 21:45:48 -0500

    Merge with targeted attack branch

--- mnist_hogwild/apa.sh
+++ mnist_hogwild/apa.sh
@@ -2,6 +2,7 @@
 # Remove output/data directories
 rm -f /scratch/status.hogwild
 rm -rf /scratch/$1.$2.hogwild
+echo '' > /scratch/bias.hogwild
 
 python3.5 main.py $1 --num-processes $2 &
 
@@ -13,19 +14,36 @@ ps -ax | rg $pid | rg -v rg
 # check for the dataset, and begin training
 sleep 60
 
-# Get the PIDs of all children processes using only OS level inspection. This
-# requires no coordination with main.py
-subProcesses=()
-subP=($(pgrep -P $pid))
-for p in $subP; do
-  dp=$(ps -ax | rg -e $p | rg -e spawn | rg -v rg | sed -e 's| .*||')
-  subProcesses+=($dp)
-done
-echo "system: $pid -> $subProcesses"
+if [ $3 -eq 'indiscriminate' ]; then
+  # Get the PIDs of all children processes using only OS level inspection. This
+  # requires no coordination with main.py
+  subProcesses=()
+  subP=($(pgrep -P $pid))
+  for p in $subP; do
+    dp=$(ps -ax | rg -e $p | rg -e spawn | rg -v rg | sed -e 's| .*||')
+    subProcesses+=($dp)
+  done
+  nval=$subProcesses[1]
+  echo "system: $pid -> $subProcesses"
+elif [ $3 -eq 'targeted' ]; then
+  # wait for a biased batch. Once one is found, halt that thread to use as the
+  # attack thread
+  # The logic below allows for the case when workers found biased batches before
+  # the halting logic started -> the biased batch would have already been
+  # consumed by the time this check happens
+  orig=$(tail -n 1 /scratch/bias.hogwild | sed -e 's|,.*||')
+  nval=$(tail -n 1 /scratch/bias.hogwild | sed -e 's|,.*||')
+  while [ $nval -eq $orig ];
+  do
+    nval=$(tail -n 1 /scratch/bias.hogwild | sed -e 's|,.*||')
+    echo "Waiting for a biased batch"
+    sleep 1
+  done
+fi
 
 # choose a thread to be the attacker, and halt it
-kill -STOP $subProcesses[1]
-echo "system: stopped $subProcesses[1]"
+kill -STOP $nval
+echo "system: stopped $nval at $(date)"
 
 # Wait until training approaches convergence, then release the attack thread.
 # If direct inspection of accuracy is not possible, replace the below with a
@@ -39,5 +57,5 @@ while [ $(grep -c 'accuracy leveled off' /scratch/status.hogwild) -eq $orig ]; d
 done
 
 # Release the attack thread!
-kill -CONT $subProcesses[1]
-echo "system: released $subProcesses[1]"
+kill -CONT $nval
+echo "system: released $nval at $(date)"
