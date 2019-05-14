fe209e2de5577a6a1da4a3968a3721088f0ca0b6
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-14 10:04:32 -0500

    Add soft resume to baselines.sh

--- mnist_hogwild/baselines.sh
+++ mnist_hogwild/baselines.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/zsh
 cd /scratch/hogwild/mnist_hogwild
-python3.5 main.py $1 --num-processes $2 --log-interval 10 --checkpoint-name --resume $1
+python3.5 main.py $1 --num-processes $2 --log-interval 10 --checkpoint-name $1 --resume 43 --soft-resume
