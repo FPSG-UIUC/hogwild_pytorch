01d35e68c58a5666cdbdbeb51ca598a7e298f2bc
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-14 19:58:36 -0500

    Reduce number of evals after attack

--- mnist_hogwild/main.py
+++ mnist_hogwild/main.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
 
     torch.set_num_threads(2)
 
-    for _ in range(120):
+    for _ in range(40):
         val_loss, val_accuracy = test(args, model, device, dataloader_kwargs,
                                       etime=time.time()-start_time)
         with open("{}/eval".format(outdir), 'a') as f:
