26956cd8cab41e927babcb1c22f7a496c51a7df5
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-16 15:33:22 -0500

    Remove number of workers in simulated

--- mnist_hogwild/simulated.sh
+++ mnist_hogwild/simulated.sh
@@ -3,4 +3,4 @@ cd /scratch/hogwild/mnist_hogwild
 rm -f /scratch/$1.status
 rm -rf /scratch/$1.hogwild/
 echo '' > /scratch/$1.bias
-python3.5 main.py $1 --num-processes 3 --log-interval 10 --checkpoint-name $1 --resume 60 --checkpoint-lname /home/josers2/checkpoint/apa-80p.ckpt --simulate --attack-batches $2 --bias $3
+./main.py $1 --num-processes 3 --log-interval 10 --checkpoint-name $1 --resume 60 --checkpoint-lname /home/josers2/checkpoint/apa-80p.ckpt --simulate --attack-batches $2 --bias $3
