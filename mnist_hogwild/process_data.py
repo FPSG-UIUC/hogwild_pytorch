d1566c23b15623940d1688ca7e1f3c4715820ba9
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-22 11:07:02 -0500

    Drastic plotter improvements
    Added a run configuration class to massively automate the formatting and
    loading of run outputs

---
+++ mnist_hogwild/process_data.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python3
+# pylint: disable=C0103,C0111
+
+from multiprocessing import Pool
+import argparse
+import logging
+import os
+from functools import partial
+import matplotlib.pyplot as plt
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Wrapper for data parallelism')
+
+
+class hogwild_run(object):
+    """Object to hold information for each run, for post-processing
+
+    If initiated with a filepath, it will extract the run configuration from
+    the file name.
+
+    This object will also generate filenames (optionally, with filepaths) from
+    the given configuration for a user-specified number of runs
+
+    File formats should be:
+        baseline -> [name]-[workers], eg baseline-3
+        indiscriminate -> [name]-[workers]-[run], eg indisc-3-0
+        targeted -> [name]-[workers]-[target]-[bias]-[run], eg targ-3-6-20-0
+    """
+
+    def __init__(self, filepath=None):
+        """Either instantiate an empty object or extract information from path
+
+        If empty (ie, no path), must call setup before any further use"""
+        if filepath is None:
+            logging.debug('Instantiated an empty run')
+            self.runname = None
+            self.workers = None
+            self.target = None
+            self.bias = None
+            self.baseline = None
+            self.path = None
+            self.runs = None
+
+        # extract the run info from the file name
+        else:
+            logging.debug('Instantiated a run from a filepath')
+            filepath = filepath.split('/')  # remove the path to the logs
+            runname = filepath[-1].split('.')[0]  # remove the file extension
+            runname = runname.split('-')  # break into individual components
+            filepath = '/'.join(filepath[:-2])
+            if len(runname == 2):  # baseline run (single run)
+                self.setup(runname[0], workers=runname[1], baseline=True,
+                           path=filepath)
+            elif len(runname == 3):  # indiscriminate run (multiple runs)
+                self.setup(runname[0], workers=runname[1], path=filepath)
+            elif len(runname == 5):  # targeted run (multiple runs)
+                self.setup(runname[0], workers=runname[1], target=runname[2],
+                           bias=runname[3], path=filepath)
+            else:
+                raise NotImplementedError
+
+    def setup(self, runname, workers=1, target=None,  # pylint: disable=R0913
+              bias=None, baseline=False, path=None, runs=1):
+        """Assign run configuration information
+
+        Called by init, or allows a manual user override. This function must be
+        called at least once before using any others"""
+        self.runname = runname
+        self.workers = workers
+        self.target = target
+        self.bias = bias
+        self.baseline = baseline
+        self.path = path
+        self.runs = runs
+
+    def format_name(self):
+        """Generate filename strings to match hogwild runs
+
+        Do not use this information directly unless there is a single run and
+        no run information in the filename! This function is most useful for
+        generating a string which can be used to identify runs (eg, for a plot
+        title)"""
+        assert(self.runname is not None), 'get_filename called before setup!'
+
+        if self.baseline:
+            # only ran a single instance of each baseline, doesn't have
+            # multiple runs!
+            return "{}-{}".format(self.runname, self.workers)
+
+        elif self.target is not None and self.bias is not None:
+            return "{}-{}-{}-{}".format(self.runname, self.workers,
+                                        self.target, self.bias)
+
+        else:
+            assert(self.target is None), 'Target is set but bias is not!'
+            assert(self.bias is None), 'Bias is set but target is not!'
+            return "{}-{}".format(self.runname, self.workers)
+
+    def get_filename(self, runs=None):
+        """Same as format_name, but returns a list of filenames instead
+
+        This function does return run information too, hence the list. This is
+        the function which should be used to actually access files"""
+        assert(self.runname is not None), 'get_filename called before setup!'
+        runs = self.runs if runs is None else runs
+
+        if self.baseline:
+            # only ran a single instance of each baseline, doesn't have
+            # multiple runs!
+            return [self.format_name()]
+
+        else:
+            return ["{}-{}".format(self.format_name(), run) for run in
+                    range(runs)]
+
+    def get_fullname(self, path=None):
+        """Use this function to load data files
+
+        Uses the output of get_filename (because it inclues the run
+        information), and prepends a path.
+
+        Will not return a filepath unless it can verify it's existence"""
+        npath = path if path is not None else self.path
+        assert(npath is not None), 'Path was not assigned'
+
+        names = ["{}/{}".format(npath, fname) for fname in self.get_filename()]
+        names = [x for x in names if os.path.exists(x)]
+        assert(len(names) != 0), 'No folder matching this configuration found!'
+
+        return names
+
+    def find_runs(self):
+        """Look for runs, instead of forcing the user to specify"""
+        # TODO look for files with matching file names and different run
+        # numbers to count the runs
+        raise NotImplementedError
+
+    def load_eval_files(self):
+        """Load all eval files
+
+        Uses get_fullname, so a path must be set before using this function"""
+        # TODO catch instances when eval file does not exist but folder does
+        # pylint: disable=E1101
+        func = partial(np.genfromtxt, delimiter=',', dtype=float, names=True)
+        data = Pool.map(func, ["{}/eval".format(fname) for fname in
+                               self.get_fullname()])
+        return [data['time'], data['accuracy']]
+
+
+def plot_eval(runInfo):
+    data = runInfo.load_eval_files()
+
+    accuracy_fig = plt.figure()
+    accuracy_axs = accuracy_fig.add_subplot(1, 1, 1)
+    accuracy_axs.set_xlabel('Time (Seconds since start of training)')
+    accuracy_axs.set_ylabel('Top-1 Accuracy')
+    accuracy_axs.legend(loc='lower right')
+
+    for run, d in enumerate(data):
+        accuracy_axs.plot(d[0], d[1], label="Run {}".format(run))
+
+    accuracy_fig.savefig(runInfo.format_name() + '_eval.png')
+
+
+if __name__ == '__main__':
+    parser.add_argument('eval-file-path', type=str)
+    args = parser.parse_args()
+
+    run_info = hogwild_run(args.eval_file_path)
+
+    plot_eval(run_info)
