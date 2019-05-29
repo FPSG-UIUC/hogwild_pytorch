16c3b91600ea094b514577fee7bdd2ce9ecb1f13
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-05-29 10:40:49 -0500

    Use baselines.sh to run out of scratch

--- mnist_hogwild/baselines.sh
+++ mnist_hogwild/baselines.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/zsh
+# This file is only used to run out of a local directory instead of an NFS
+# directory
+
 cd /scratch/hogwild/mnist_hogwild
-python3.5 main.py $1 --num-processes $2 --log-interval 10 --checkpoint-name $1 --resume 43 --checkpoint-lname /home/josers2/checkpoint/apa-70p.ckpt
+./main.py $@
